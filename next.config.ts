36555b84726c74f3dff011989be5b920eaa02f3d
Jeerapol <Jeerapol.dev@gmail.com>
2026-02-06 18:31:16 +0700

    feat: Set up GitHub Pages deployment with static export configuration for Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/pai_nai_dee",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
