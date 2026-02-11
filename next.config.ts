18e10f3021e8f8379ef4b677042c7dcbf3b47d0e
Jeerapol <Jeerapol.dev@gmail.com>
2026-02-11 13:51:08 +0700

    config: Add conditional `NEXT_PUBLIC_BASE_PATH` environment variable.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/pai_nai_dee" : "",
+  },
 };
 
 export default nextConfig;
