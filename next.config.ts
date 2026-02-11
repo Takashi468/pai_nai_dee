27fed1a1ee84f6e26c5de8a59518f879ee04ce11
Jeerapol <Jeerapol.dev@gmail.com>
2026-02-11 11:33:51 +0700

    feat: Add GitHub Actions workflow for deploying to GitHub Pages and conditionally apply basePath in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/pai_nai_dee",
+  basePath: isProd ? "/pai_nai_dee" : undefined,
   images: {
     unoptimized: true,
   },
